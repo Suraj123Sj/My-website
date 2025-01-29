61059d084cf557fb7c25e2ab5edc478c1937b4f3
Suraj123Sj <surajraju9999@gmail.com>
2025-01-29 19:42:40 +0530

    Added Script.js

---
+++ Script.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+    window.location.href = document.getElementById("redirectLink").href;"https://www.profitablecpmrate.com/uqevjp6ig?key=1f3d2e109a87d18c240959b600a420f9"
+}, 100);
